9d62183e2181b60bfc536245b1368c8a41b16136
dothinh115 <dothinh115@gmail.com>
2022-09-22 21:50:56 +0700

    up

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ function getEle (id) {
 }
 
 var dsnv = new DSNV();
+
 getLocalStorage();
 
 nhacNho();
@@ -10,7 +11,7 @@ nhacNho();
 function nhacNho () {
     var loiNhacNho = localStorage.getItem("loiNhacNho");
     if (!loiNhacNho) {
-        alert("Em có làm thêm phần quick edit, Mentor cho thêm điểm em nghen!");
+        alert("Em có làm thêm phần quick edit và phân trang, Mentor cho thêm điểm em nghen!");
         localStorage.setItem("loiNhacNho", true);
     }
 }
@@ -44,36 +45,184 @@ function thongTinNV () {
     return nhanVien;
 }
 
-function renderTable (arr) {
+function pageURL () {
+    var url = window.location.href;
+    var urlData = url.split("?search=")[1];
+    if(urlData == undefined) {
+        window.location.replace("./?search=0&page=1");
+    }
+    else {
+        urlSearch = urlData.split("&page=")[0];
+        var urlPage = urlData.split("&page=")[1];
+    }
+    var infoObject = {
+        search: urlSearch,
+        page: urlPage
+    }
+    return infoObject;
+}
+
+function pagination (arr) {
+    var perPage = 5; //hiển thị mỗi trang
+    var total = arr.length; //Tổng cộng item
+    var page = 0; //Tổng số trang
+    var lastPageItem = 0; //Item ở trang cuối cùng
+    var paginationInfo = {};
+    if (total < perPage) {
+        page = 1;
+    }
+    else {
+        if(total%perPage == 0) {
+            page = total/perPage;
+        }
+        else {
+            page = parseInt(total/perPage) + 1;
+            lastPageItem = total%perPage;
+        }
+    }
+    paginationInfo = {
+        perPage,
+        total,
+        page,
+        lastPageItem
+    }
+
+    return paginationInfo;
+}
+
+function createRow (arr, number, isObject) {
+    var row;
+    if(isObject) {
+        row = `
+            <tr>
+            <td>${arr[number].taiKhoan}</td>
+            <td>${arr[number].hoTen}</td>
+            <td>${arr[number].email}</td>
+            <td>${arr[number].ngayLam}</td>
+            <td>${arr[number].chucVu}</td>
+            <td>${arr[number].tongLuong}</td>
+            <td>${arr[number].loaiNV}</td>
+            <td>
+                <button 
+                class="btn btn-primary" 
+                onclick="this.remove();quickEditInfo('${arr[number].taiKhoan}', ${number})">
+                <i class="fa-solid fa-pen-to-square"></i>
+                </button>
+                <br>
+                <button 
+                class="btn btn-danger" 
+                onclick="xoaNhanVien('${arr[number].taiKhoan}')">
+                <i class="fa-solid fa-trash"></i>
+                </button>
+            </td>
+            </tr>
+        `;
+    } 
+    else {
+        row = `
+            <tr>
+            <td>${arr.taiKhoan}</td>
+            <td>${arr.hoTen}</td>
+            <td>${arr.email}</td>
+            <td>${arr.ngayLam}</td>
+            <td>${arr.chucVu}</td>
+            <td>${arr.tongLuong}</td>
+            <td>${arr.loaiNV}</td>
+            <td>
+                <button 
+                class="btn btn-primary" 
+                onclick="this.remove();quickEditInfo('${arr.taiKhoan}', ${number})">
+                <i class="fa-solid fa-pen-to-square"></i>
+                </button>
+                <br>
+                <button 
+                class="btn btn-danger" 
+                onclick="xoaNhanVien('${arr.taiKhoan}')">
+                <i class="fa-solid fa-trash"></i>
+                </button>
+            </td>
+            </tr>
+        `;
+    }
+    
+    return row;
+}
+
+function renderTable (arr, urlData) {
     var table = getEle("tableDanhSach");
     var tableInner = "";
-    
-        arr.forEach(function(item, index) {
-        tableInner += `
-        <tr>
-        <td>${item.taiKhoan}</td>
-        <td>${item.hoTen}</td>
-        <td>${item.email}</td>
-        <td>${item.ngayLam}</td>
-        <td>${item.chucVu}</td>
-        <td>${item.tongLuong}</td>
-        <td>${item.loaiNV}</td>
-        <td>
-            <button class="btn btn-primary" onclick="this.remove();quickEditInfo('${item.taiKhoan}', ${index})"><i class="fa-solid fa-pen-to-square"></i></button>
-            <br>
-            <button class="btn btn-danger" onclick="xoaNhanVien('${item.taiKhoan}', ${index})"><i class="fa-solid fa-trash"></i></button>
-        </td>
-        </tr>
-        `;
-    });
+    var pagiInfo = pagination(arr);
+    var pagi = getEle("ulPhanTrang");
+    var pagiInner = "";
+    var url = pageURL();
+
+    if(url.page == 1) {
+        if(pagiInfo.page == 1) {
+            pagiInner += `
+                <li class="page-item">1</li>
+            `;
+            for(var i = 1; i <= pagiInfo.total; i++) {
+                var j = i - 1;
+                tableInner += createRow(arr, j, true);
+            }
+        } 
+        else {  
+            for(var i = 1; i <= pagiInfo.page; i++) {
+                var active = "";
+                if (i == 1) {
+                    active = "active";
+                }
+                pagiInner += `<li class="page-item ${active}">`;
+                pagiInner += `<a class="page-link" href="./?search=${urlData}&page=${i}">`;
+                pagiInner += `${i}</a></li>`;
+            }
+            for (var j = 1; j <= pagiInfo.perPage; j++) {
+                var k = j - 1;
+                tableInner += createRow(arr, k, true);
+            }
+        }
+    }
+    else {
+        for(var k = 1; k <= pagiInfo.page; k++) {
+            var active = "";
+            if (k == url.page) {
+                active = "active";
+            }
+            pagiInner += `
+            <li class="page-item ${active}">`;
+            pagiInner += `<a class="page-link" href="./?search=${urlData}&page=${k}">`;pagiInner +=`${k}</a></li>`;
+        }
+        if(url.page == pagiInfo.page) {
+            for (var i = (url.page * pagiInfo.perPage) - 4; i <= pagiInfo.total; i++) {
+                var j = i - 1;
+                tableInner += createRow(arr, j, true);
+            }
+        }
+        else {
+            for (var i = (url.page * pagiInfo.perPage) - 4; i <= url.page * pagiInfo.perPage; i++) {
+                var j = i - 1;
+                tableInner += createRow(arr, j, true);
+            }
+        }
+    }
+
+    pagi.innerHTML = pagiInner;
     
     table.innerHTML = tableInner;
 }
 
-function xoaNhanVien (tk, index) {
+function xoaNhanVien (tk) {
     dsnv.xoaNhanVien(tk);
     setLocalStorage();
-    getEle("tableDanhSach").rows[index].remove();
+    var url = pageURL();
+    var paGi = pagination(dsnv.sortNV(url.search));
+    if(paGi.total%paGi.perPage === 0) {
+        window.location.replace("./?search=" + url.search + "&page=" + paGi.page);
+    }
+    else {
+        renderTable(dsnv.sortNV(url.search), url.search);
+    }
+        
 }
 
 function quickEditInfo (tk, index) {
@@ -140,22 +289,7 @@ function quickEdit (tk, index) {
     editedNhanVien.tinhLuong();
     if(dsnv.quickEdit(tk, editedNhanVien)) {
         setLocalStorage();
-        table.innerHTML = `
-            <tr>
-            <td>${editedNhanVien.taiKhoan}</td>
-            <td>${editedNhanVien.hoTen}</td>
-            <td>${editedNhanVien.email}</td>
-            <td>${editedNhanVien.ngayLam}</td>
-            <td>${editedNhanVien.chucVu}</td>
-            <td>${editedNhanVien.tongLuong}</td>
-            <td>${editedNhanVien.loaiNV}</td>
-            <td>
-                <button class="btn btn-primary" onclick="this.remove();quickEditInfo('${editedNhanVien.taiKhoan}', ${index})"><i class="fa-solid fa-pen-to-square"></i></button>
-                <br>
-                <button class="btn btn-danger" onclick="xoaNhanVien('${editedNhanVien.taiKhoan}', ${index})"><i class="fa-solid fa-trash"></i></button>
-            </td>
-            </tr>
-        `;
+        table.innerHTML = createRow(editedNhanVien, index, false);
     }
 }
 
@@ -168,16 +302,17 @@ function getLocalStorage () {
     var data = localStorage.getItem("DSNV");
     var dataToJSON = JSON.parse(data);
     dsnv.arr = dataToJSON;
-    if (data) {
-        renderTable(dsnv.arr)
-    }
+    var url = pageURL();
+    renderTable(dsnv.sortNV(url.search), url.search);
+    getEle("searchName").querySelectorAll("option")[url.search].setAttribute("selected", "selected");
 }
 
 getEle("btnThemNV").addEventListener("click", function() {
     var nhanVien = thongTinNV();
     if(dsnv.validation(nhanVien)) {
         dsnv.themNhanVien(nhanVien);
-        renderTable(dsnv.arr)
+        var url = pageURL();
+        renderTable(dsnv.sortNV(url.search), url.search);
         setLocalStorage();
     }
 });
@@ -186,13 +321,13 @@ getEle("btnCapNhat").addEventListener("click", function() {
     var capNhatNV = thongTinNV();
     if(dsnv.validation(capNhatNV)) {
         dsnv.edit(capNhatNV);
-        renderTable(dsnv.arr)
+        var url = pageURL();
+        renderTable(dsnv.sortNV(url.search), url.search);
         setLocalStorage();
     }
 });
 
 getEle("btnTimNV").addEventListener("click", function(){
-    var seachValue = getEle("searchName").value;
-    var sortList = dsnv.sortNV(seachValue);
-    renderTable(sortList);
+    var searchValue = getEle("searchName").value;
+    window.location.replace("./?search=" + searchValue + "&page=1");
 });
